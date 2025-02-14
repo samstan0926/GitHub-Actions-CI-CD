bcb646d7e9d0e1d66f2a6b8004cde1e2e676f895
samstan092 <samstan092@gmail.com>
2025-02-14 15:24:11 -0600

    e

--- Develop/server/src/seeds/seed.ts
+++ Develop/server/src/seeds/seed.ts
@@ -1,11 +1,11 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
-import cleanDB from "./cleanDb.js";
+
 
 import pythonQuestions from './pythonQuestions.json' with { type: "json" };
 
 db.once('open', async () => {
-  await cleanDB('Question', 'questions');
+
 
   await Question.insertMany(pythonQuestions);
 
